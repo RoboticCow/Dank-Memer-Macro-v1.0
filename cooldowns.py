7dd3de4c629975653267ba0467af02e14fac5af7
RoboticCow <93643965+RoboticCow@users.noreply.github.com>
2022-03-13 04:07:42 +0800

    Update cooldowns.py

--- cooldowns.py
+++ cooldowns.py
@@ -1,17 +1,13 @@
-'''
-cooldowns mentioned below (in seconds) are the minimum. For the sake of randomness, there will be random 1-5sec cooldown added to these.
-Note: the pink cds always vary and can depend on the bot, feel free to change them how you see appropraite
-'''
-
-
-beg_cooldown= 45
-fish_cooldown= 40
-hunt_cooldown= 40
-pm_cooldown= 45
+beg_cooldown = 45
+fish_cooldown = 40
+hunt_cooldown = 40
+pm_cooldown = 45
 pmAnswer_cooldown = 2
-hlAnswer_cooldown= 3
-hl_cooldown= 11
-buy_cooldown= 5
+hlAnswer_cooldown = 3
+hl_cooldown = 11
+buy_cooldown = 5
 dig_cooldown = 40
 scout_cooldown = 30
-dep_cooldown=1200
+dep_cooldown = 1200
+crime_cooldown = 45
+horseshoe_cooldown = 900
